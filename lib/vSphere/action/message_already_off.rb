2e611487435e4242b404a68d12f769b37e9d75af
Matt Callaway <mcallawa@genome.wustl.edu>
2013-10-08 17:10:05 -0500

    Add off and running messages.  Fix not created.

--- lib/vSphere/action/message_already_off.rb
+++ lib/vSphere/action/message_already_off.rb
@@ -3,13 +3,13 @@ require 'i18n'
 module VagrantPlugins
   module VSphere
     module Action
-      class MessageNotCreated
+      class MessageAlreadyOff
         def initialize(app, env)
           @app = app
         end
 
         def call(env)
-          env[:ui].info I18n.t('vsphere.vm_not_created')
+          env[:ui].info I18n.t('vsphere.vm_already_off')
           @app.call(env)
         end
       end
--- lib/vSphere/action/message_already_running.rb
+++ lib/vSphere/action/message_already_running.rb
@@ -3,13 +3,13 @@ require 'i18n'
 module VagrantPlugins
   module VSphere
     module Action
-      class MessageNotCreated
+      class MessageAlreadyRunning
         def initialize(app, env)
           @app = app
         end
 
         def call(env)
-          env[:ui].info I18n.t('vsphere.vm_not_created')
+          env[:ui].info I18n.t('vsphere.vm_already_running')
           @app.call(env)
         end
       end
