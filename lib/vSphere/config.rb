65d12197d9cf1b3984f983f406e906c4b5066800
Matt Callaway <mcallawa@genome.wustl.edu>
2013-10-08 14:37:14 -0500

    Fix whitespace. Add target folder.  We don't want to put VMs in the templates folder.

--- lib/vSphere/config.rb
+++ lib/vSphere/config.rb
@@ -4,26 +4,29 @@ module VagrantPlugins
   module VSphere
     class Config < Vagrant.plugin('2', :config)
       attr_accessor :host
-      attr_accessor :insecure
       attr_accessor :user
       attr_accessor :password
       attr_accessor :data_center_name
       attr_accessor :compute_resource_name
       attr_accessor :resource_pool_name
       attr_accessor :template_name
+      attr_accessor :target_folder
       attr_accessor :name
+      attr_accessor :insecure
+      attr_accessor :debug
 
       def validate(machine)
         errors = _detected_errors
 
         #TODO: add blank?
         errors << I18n.t('config.host') if host.nil?
-        errors <<  I18n.t('config.user') if user.nil?
-        errors <<  I18n.t('config.password') if password.nil?
-        errors <<  I18n.t('config.name') if name.nil?
-        errors<<  I18n.t('config.template') if template_name.nil?
+        errors << I18n.t('config.user') if user.nil?
+        errors << I18n.t('config.password') if password.nil?
+        errors << I18n.t('config.name') if name.nil?
+        errors << I18n.t('config.template') if template_name.nil?
         errors << I18n.t('config.compute_resource') if compute_resource_name.nil?
         errors << I18n.t('config.resource_pool') if resource_pool_name.nil?
+        errors << I18n.t('config.target_folder') if target_folder.nil?
 
         { 'vSphere Provider' => errors }
       end
